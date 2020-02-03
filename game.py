953be3265f4b238b90ac9e0c746b7d87b148644d
Mike Rossetti <s2t2@users.noreply.github.com>
2020-02-03 18:17:40 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,18 +1,59 @@
 
 # game.py
 
+import random
+
 print("Rock, Paper, Scissors, Shoot!")
 
 # Processing User Inputs
 
-x = input("Please choose either 'rock', 'paper', or 'scissors':   ")
-print("USER SAYS...", x)
+user_choice = input("Please choose either 'rock', 'paper', or 'scissors': ")
+print("USER SAYS...", user_choice)
 
 # Validating User Inputs
 
+valid_options = ["rock", "paper", "scissors"]
+
+if user_choice in valid_options:
+    print("VALID")
+else:
+    print("INVALID")
+    exit()
+
 # Simulating Computer Selection
 
+print("COMPUTER CHOICE:")
+computer_choice = random.choice(valid_options)
+print(computer_choice)
+
 # Determining the Winner
+#
+# Rock beats Scissors
+# Paper beats Rock
+# Scissors beats Paper
+# Rock vs Rock, Paper vs Paper, and Scissors vs Scissors each results in a "tie"
+
+if computer_choice.lower() == user_choice.lower():
+    print("TIE")
+elif computer_choice.lower() == "rock":
+    if user_choice.lower() == "scissors":
+        print("COMP WINS")
+    elif user_choice.lower() == "paper":
+        print("USER WINS")
+elif computer_choice.lower() == "scissors":
+    if user_choice.lower() == "paper":
+        print("COMP WINS")
+    elif user_choice.lower() == "rock":
+        print("USER WINS")
+elif computer_choice.lower() == "paper":
+    if user_choice.lower() == "rock":
+        print("COMP WINS")
+    elif user_choice.lower() == "scissors":
+        print("USER WINS")
+else:
+    print("TODO determine winner")
+
+
 
 # Displaying Results
 #
