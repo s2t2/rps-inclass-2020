c379946d37117b4a913ad4f013dd59902b1d3757
Mike Rossetti <s2t2@users.noreply.github.com>
2020-02-03 18:01:03 -0500

    Create game and ask for user input

---
+++ game.py
@@ -0,0 +1,28 @@
+
+# game.py
+
+print("Rock, Paper, Scissors, Shoot!")
+
+# Processing User Inputs
+
+x = input("Please choose either 'rock', 'paper', or 'scissors':   ")
+print("USER SAYS...", x)
+
+# Validating User Inputs
+
+# Simulating Computer Selection
+
+# Determining the Winner
+
+# Displaying Results
+#
+# -------------------
+# Welcome to my Rock-Paper-Scissors game...
+# -------------------
+# Please choose either 'rock', 'paper', or 'scissors': rock
+# You chose: 'rock'
+# The computer chose: 'paper'
+# -------------------
+# Oh, the computer won. It's ok.
+# -------------------
+# Thanks for playing. Please play again!
